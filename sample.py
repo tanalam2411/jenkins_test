1387a2aeff099ffce662c3da6343e4786b86ad57
Tanveer <tan24alam11@gmail.com>
2015-04-24 01:07:41 +0530

    sample with unit

--- sample.py
+++ sample.py
@@ -2,6 +2,24 @@
 
 
 
-with open("C:\Users\tanveer\Desktop\phanneendra Sir\Jenkins And Git\Git Tutorials\git_example\jenkins_1.txt", "w") as fp:
-	fp.write("git jenkins")
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+  def test_upper(self):
+      self.assertEqual('foo'.upper(), 'FOO')
+
+  def test_isupper(self):
+      self.assertTrue('FOO'.isupper())
+      self.assertFalse('Foo'.isupper())
+
+  def test_split(self):
+      s = 'hello world'
+      self.assertEqual(s.split(), ['hello', 'world'])
+      # check that s.split fails when the separator is not a string
+      with self.assertRaises(TypeError):
+          s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
 	
